03796076c490de18abadcd1aaf9b4648b872f08e
stepantsuk <stepan.tsukanov@yandex.ru>
2022-07-11 14:08:22 +0300

    small fixes

--- script.js
+++ script.js
@@ -3,6 +3,31 @@ const addTaskBtn = document.querySelector(".add__task");
 const tasksList = document.querySelector(".todo__list");
 const counterValue = document.querySelector(".counter__value");
 
+addTaskBtn.addEventListener("click", (e) => {
+  e.preventDefault;
+  if (inputTask.value) {
+    putTaskInArr();
+    updateLocalStorage();
+    fillTemplate();
+  } else {
+    alert("значение не может быть пустым");
+  }
+});
+
+inputTask.addEventListener("keydown", (e) => {
+  e.preventDefault;
+  if (e.keyCode !== 13) {
+    return null;
+  }
+  if (inputTask.value) {
+    putTaskInArr();
+    updateLocalStorage();
+    fillTemplate();
+  } else {
+    alert("значение не может быть пустым");
+  }
+});
+
 let todoArr = [];
 //проверим есть ли в локалсторедж таски, если да, то наполним массив
 localStorage.tasks
@@ -27,27 +52,27 @@ const updateLocalStorage = () =>
   localStorage.setItem("tasks", JSON.stringify(todoArr));
 
 //отсортируем массив на выполненный или нет
-function filterTasks() {
-  let completedTasks =
+const filterTasks = () => {
+  const completedTasks =
     todoArr.length && todoArr.filter((item) => item.completed == true);
-  let unCompletedTasks =
+  const unCompletedTasks =
     todoArr.length && todoArr.filter((item) => item.completed == false);
   todoArr = [...unCompletedTasks, ...completedTasks];
-}
+};
 
 //создадим функцию, которая создает темплейт
-let createTemplate = (task, index) => {
-  let li = document.createElement("li");
+const createTemplate = (task, index) => {
+  const li = document.createElement("li");
   li.classList.add("todo__item");
 
-  let div = document.createElement("div");
+  const div = document.createElement("div");
   div.classList.add("todo__text");
   div.innerText = task;
   todoArr[index].completed
     ? div.classList.add("todo__item-active")
     : div.classList.remove("todo__item-active");
 
-  let checkbox = document.createElement("input");
+  const checkbox = document.createElement("input");
   checkbox.classList.add("todo__complete");
   checkbox.setAttribute("type", "checkbox");
   todoArr[index].completed
@@ -59,7 +84,7 @@ let createTemplate = (task, index) => {
     fillTemplate();
   });
 
-  let del = document.createElement("button");
+  const del = document.createElement("button");
   del.innerText = "Удалить";
   del.classList.add("delete__task");
   del.addEventListener("click", (e) => {
@@ -74,7 +99,7 @@ let createTemplate = (task, index) => {
   tasksList.append(li);
 };
 
-const fillTemplate = function () {
+const fillTemplate = () => {
   tasksList.innerHTML = "";
   if (todoArr.length > 0) {
     filterTasks();
@@ -85,26 +110,4 @@ const fillTemplate = function () {
 
 fillTemplate();
 
-addTaskBtn.addEventListener("click", (e) => {
-  e.preventDefault;
-  if (inputTask.value) {
-    putTaskInArr();
-    updateLocalStorage();
-    fillTemplate();
-  } else {
-    alert("значение не может быть пустым");
-  }
-});
 
-inputTask.addEventListener("keydown", function (e) {
-  if (e.keyCode === 13) {
-    e.preventDefault;
-    if (inputTask.value) {
-      putTaskInArr();
-      updateLocalStorage();
-      fillTemplate();
-    } else {
-      alert("значение не может быть пустым");
-    }
-  }
-});
